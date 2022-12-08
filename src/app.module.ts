939d60f09114b2e6a5c9bf50793ba12771f79338
h.yoshino <osushicrusher@gmail.com>
2022-12-08 11:08:58 +0900

    feat:created Task Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TaskModule } from './tasks/task.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432, // 開発用コンテナのポートを指定
+      username: 'root',
+      password: 'secret',
+      database: 'mydb-dev', // 開発用のデータベース名を指定
+      autoLoadEntities: true, // true とすると、Entity が自動的に読み込まれます
+      synchronize: true, // true とすると、アプリケーションを起動時に自動でテーブルが作成されます
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
